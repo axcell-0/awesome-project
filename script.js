4211484d21abecd12389a275ab5fc1050b012d25
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-07-11 12:32:52 +0100

    Add refactor sections and functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const books = JSON.parse(localStorage.getItem('books')) || [];
+const few = document.getElementsByTagName('a');
+const sec = document.getElementsByTagName('section');
+const datedis = document.querySelector('.currentDate');
+
 
 function displayBooks() {
   const booksDiv = document.getElementById('books');
@@ -57,3 +61,41 @@ addBookForm.addEventListener('submit', (event) => {
 });
 
 displayBooks();
+
+// Display sections according to the nav elements
+window.addEventListener('DOMContentLoaded', () => {
+  sec[0].classList.toggle('active');
+});
+
+for (let i = 0; i < few.length; i += 1) {
+  few[i].addEventListener('click', () => {
+    for (let j = 0; j < sec.length; j += 1) {
+      if (i === j) {
+        sec[j].style.display = 'block';
+      } else {
+        sec[j].style.display = 'none';
+      }
+    }
+  });
+}
+
+// Adding current date to the page.
+const d = new Date();
+const year = d.getFullYear();
+let date = d.getDate();
+const hours = d.getHours();
+let apm;
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+if (date === 1 || date === 21 || date === 31) {
+  date += 'st';
+} else if (date === 2 || date === 22) {
+  date = `${date}nd`;
+} else {
+  date = `${date}th`;
+}
+if (hours < 12) {
+  apm = 'am';
+} else {
+  apm = 'pm';
+}
+datedis.innerHTML = `${monthNames[d.getMonth()]} ${date} ${year}, ${hours}:${d.getMinutes()}:${d.getSeconds()} ${apm}`;
