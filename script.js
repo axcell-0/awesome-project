197a3d7a773b95e08df13f3a3b64ffb72389dad2
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-07-11 16:22:29 +0100

    Add refactor sections and functionality

--- script.js
+++ script.js
@@ -1,101 +1,77 @@
-const books = JSON.parse(localStorage.getItem('books')) || [];
-const few = document.getElementsByTagName('a');
-const sec = document.getElementsByTagName('section');
-const datedis = document.querySelector('.currentDate');
+class Library {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.booksDiv = document.getElementById('books');
+    this.addBookForm = document.getElementById('addBookForm');
+    this.titleInput = document.getElementById('titleInput');
+    this.authorInput = document.getElementById('authorInput');
 
-
-function displayBooks() {
-  const booksDiv = document.getElementById('books');
-  booksDiv.innerHTML = '';
-
-  if (books.length === 0) {
-    booksDiv.textContent = 'No books in the collection.';
-    return;
+    this.addBookForm.addEventListener('submit', this.handleFormSubmit.bind(this));
+    this.displayBooks();
   }
 
-  const removeBook = (index) => {
-    books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(books));
-    displayBooks();
-  };
+  displayBooks() {
+    this.booksDiv.innerHTML = '';
 
-  books.forEach((book, index) => {
-    booksDiv.innerHTML += `
-    <div class="book">
-        <span class="book-title">${book.title}</span>
-        <span class="book-author">by ${book.author}</span>
-        <button class="remove-button" type="button" data-index="${index}">Remove</button>
-      </div>`;
-  });
-
-  const removeButtons = document.getElementsByClassName('remove-button');
-  Array.from(removeButtons).forEach((button, index) => {
-    button.addEventListener('click', () => {
-      removeBook(index);
-    });
-  });
-}
-
-function addBook(title, author) {
-  const book = {
-    title,
-    author,
-  };
-
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
-
-const addBookForm = document.getElementById('addBookForm');
-addBookForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const titleInput = document.getElementById('titleInput');
-  const authorInput = document.getElementById('authorInput');
-
-  addBook(titleInput.value, authorInput.value);
-
-  titleInput.value = '';
-  authorInput.value = '';
-});
-
-displayBooks();
-
-// Display sections according to the nav elements
-window.addEventListener('DOMContentLoaded', () => {
-  sec[0].classList.toggle('active');
-});
-
-for (let i = 0; i < few.length; i += 1) {
-  few[i].addEventListener('click', () => {
-    for (let j = 0; j < sec.length; j += 1) {
-      if (i === j) {
-        sec[j].style.display = 'block';
-      } else {
-        sec[j].style.display = 'none';
-      }
+    if (this.books.length === 0) {
+      this.booksDiv.textContent = 'No books in the collection.';
+      return;
     }
-  });
+
+    this.books.forEach((book, index) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book');
+
+      const titleSpan = document.createElement('span');
+      titleSpan.classList.add('book-title');
+      titleSpan.textContent = book.title;
+
+      const authorSpan = document.createElement('span');
+      authorSpan.classList.add('book-author');
+      authorSpan.textContent = `by ${book.author}`;
+
+      const removeButton = document.createElement('button');
+      removeButton.classList.add('remove-button');
+      removeButton.setAttribute('type', 'button');
+      removeButton.setAttribute('data-index', index);
+      removeButton.textContent = 'Remove';
+
+      removeButton.addEventListener('click', () => {
+        this.removeBook(index);
+      });
+
+      bookDiv.appendChild(titleSpan);
+      bookDiv.appendChild(authorSpan);
+      bookDiv.appendChild(removeButton);
+      this.booksDiv.appendChild(bookDiv);
+    });
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  addBook(title, author) {
+    const book = {
+      title,
+      author,
+    };
+
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    this.addBook(this.titleInput.value, this.authorInput.value);
+
+    this.titleInput.value = '';
+    this.authorInput.value = '';
+  }
 }
 
-// Adding current date to the page.
-const d = new Date();
-const year = d.getFullYear();
-let date = d.getDate();
-const hours = d.getHours();
-let apm;
-const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-if (date === 1 || date === 21 || date === 31) {
-  date += 'st';
-} else if (date === 2 || date === 22) {
-  date = `${date}nd`;
-} else {
-  date = `${date}th`;
-}
-if (hours < 12) {
-  apm = 'am';
-} else {
-  apm = 'pm';
-}
-datedis.innerHTML = `${monthNames[d.getMonth()]} ${date} ${year}, ${hours}:${d.getMinutes()}:${d.getSeconds()} ${apm}`;
+const library = new Library();
